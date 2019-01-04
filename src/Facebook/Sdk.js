8b6b8d5042ca56c4c8edfbb5467d7e3a415da02d
Valtteri Pajunen <valtteri.pajunen@ksfmedia.fi>
2019-01-04 13:12:36 +0200

    Check if FB is already initialized
    This may happen for instance if the Facebook SDK js is loaded elsewhere.

--- src/Facebook/Sdk.js
+++ src/Facebook/Sdk.js
@@ -43,20 +43,26 @@ exports._api = function(fb, path, method, params, callback) {
 exports._init = function(callback) {
   return function(fbConfig) {
     return function() { // returns an effect
-      window.fbAsyncInit = function() {
-        FB.init(fbConfig);
-        FB.AppEvents.logPageView();
-        callback(FB)(); // callback itself returns an effect
+      // Check if FB is already initialized
+      if (window.isFBLoaded || window.FB && typeof window.FB === 'object') {
+        callback(window.FB)();
+      } else {
+        window.fbAsyncInit = function() {
+          window.FB.init(fbConfig);
+          window.FB.AppEvents.logPageView();
+          window.isFBLoaded = true;
+          callback(window.FB)(); // callback itself returns an effect
+        };
+        (function(d, s, id){
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) {return;}
+          js = d.createElement(s);
+          js.id = id;
+          var debug = fbConfig['debug'] ? "/debug" : "";
+          js.src = "//connect.facebook.net/" + fbConfig['locale'] + "/sdk" + debug + ".js";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(window.document, 'script', 'facebook-jssdk'));
       };
-      (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s);
-        js.id = id;
-        var debug = fbConfig['debug'] ? "/debug" : "";
-        js.src = "//connect.facebook.net/" + fbConfig['locale'] + "/sdk" + debug + ".js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(window.document, 'script', 'facebook-jssdk'));
     }
   }
 }
